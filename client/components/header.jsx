48a7c88afd3e9c2e0694622135f7ee30c83606e3
Austin Imbastari <austinimbastari@gmail.com>
2020-11-11 20:57:04 +0000

    created the event component for the icons so the user can click

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -8,7 +8,7 @@ function Headers(props) {
           <img src="images/ui-icons/back-arrow.svg" alt="back arrow"/>
         </div>
         <div className="title-container">
-          <h1 className="nav-title">Home</h1>
+          <h1 className="nav-title">{props.name}</h1>
         </div>
         <div className="burger-container">
           <img src="images/ui-icons/burger-icon.svg" alt="burger menu"/>
