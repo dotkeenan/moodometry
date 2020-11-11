758988037c5984c4d35d358f15af38ee4da40c5f
Keenan Ng <keenandng@gmail.com>
2020-11-11 13:12:45 -0800

    created create-entry.jsx and event-details.jsx | not finished yet | added a conditional in app.jsx render() for event-details

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import Header from './header.jsx';
-import EntryList from './entry-list.jsx';
-import Nav from './nav.jsx';
+import Header from './header';
+import EntryList from './entry-list';
+import Nav from './nav';
+import CreateEntry from './create-entry';
+import EventDetails from './event-details';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       view: {
-        name: 'entries'
-      }
+        name: 'createEntry'
+      },
+      entries: []
       // message: null,
       // isLoading: true
+      // createdEntry: {};
     };
+    this.addEntry = this.addEntry.bind(this);
   }
 
   componentDidMount() {
@@ -23,18 +28,44 @@ export default class App extends React.Component {
     //   .finally(() => this.setState({ isLoading: false }));
   }
 
+  // way to get the data from the the entry form
+  addEntry(entry) {
+    const reqOptions = {
+      method: 'POST',
+      body: JSON.stringify(entry),
+      headers: { 'Content-Type': 'application/json' }
+    };
+    fetch('/api/entries', reqOptions)
+      .then(result => result.json())
+      .then(result => {
+        const updatedEntries = this.state.entries.slice();
+        updatedEntries.push(result);
+        this.setState({ entries: updatedEntries });
+      })
+      .catch(err => console.error(err));
+  }
+
+  setView(name) {
+    this.setState({
+      view: name
+    });
+  }
+
   render() {
+    let view = null;
+    if (this.state.view.name === 'entries') {
+      view = <EntryList />;
+    } else if (this.state.view.name === 'createEntry') {
+      view = <CreateEntry setView={this.setView}/>;
+    } else if (this.state.view.name === 'eventDetails') {
+      view = <EventDetails setView={this.setView}/>;
+    }
+
     return (
       <>
         <Header />
-        <div className="entry-list">
-          <div className="container entry-container">
-            <div className="row entry-row">
-              <EntryList />
-            </div>
-          </div>
-        </div>
-        <Nav />
+        {view}
+        <Nav setView={this.setView}/>
       </>
     );
   }
--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-function Nav() {
+// possibly convert into a class since there will be lots of methods
+function Nav(props) {
+  function handleAddClick() {
+    props.setView('createEntry');
+  }
+
   return (
     <div className="nav nav-row">
 
@@ -12,7 +17,7 @@ function Nav() {
         <img src="/images/ui-icons/stats.svg" alt="stats" />
       </div>
 
-      <div className="add-entry-button">
+      <div onClick={handleAddClick} className="add-entry-button">
         <div className="button-icon">
           <img src="/images/ui-icons/add.svg" alt="add" />
         </div>
