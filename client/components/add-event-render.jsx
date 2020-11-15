d928a13f5bf7a83378d95657de008650b67e448a
Keenan Ng <keenandng@gmail.com>
2020-11-15 14:05:39 -0800

    fixed issues where long messages or participants would render beneath the plus sign.

--- client/components/add-event-render.jsx
+++ client/components/add-event-render.jsx
@@ -23,14 +23,14 @@ function AddEventRender(props) {
               setPhase={props.setPhase}
             />
           </div>
-          <div className="add-field">
+          <div className="add-field row align-items-start form-margin-fix">
             <img className="hover-pointer" onClick={handleAddParticipants} src="/images/ui-icons/add-detail.svg" alt="add detail" />
-            <span className="selected-participants hover-pointer" onClick={handleAddParticipants}>{props.entryState.participants}</span>
+            <span className="selected-participants hover-pointer margin-left-10" onClick={handleAddParticipants}>{props.entryState.participants}</span>
             {/* <span className="selected-participants">Add Participants</span> */}
           </div>
-          <div className="add-field">
+          <div className="add-field row align-items-start form-margin-fix">
             <img className="hover-pointer" onClick={handleAddNote} src="/images/ui-icons/add-detail.svg" alt="add detail" />
-            <span className="selected-participants hover-pointer" onClick={handleAddNote}>{props.entryState.note}</span>
+            <span className="selected-participants hover-pointer margin-left-10" onClick={handleAddNote}>{props.entryState.note}</span>
             {/* <span className="selected-participants">Add a note</span> */}
           </div>
         </div>
